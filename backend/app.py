6ec78faa4469465f47b98107c2dd55dd198a790f
Emmanuelgessessew <163059051+Emmanuelgessessew@users.noreply.github.com>
2024-11-22 11:45:22 +0000

    setting up  the backend

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    
     return render_template('index.html')
 
 @app.route('/login')
@@ -21,6 +22,7 @@ def employee():
 @app.route('/employer')
 def employer():
     return render_template('employer.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
